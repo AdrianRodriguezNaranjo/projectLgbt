ef62cabd1218eac2ea56881cbc61a7b77a8796e0
Naidr <adrianrodrigueznaranjo@alumno.ieselrincon.es>
2023-05-01 20:14:17 +0100

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,8 +4,7 @@ import logo from "../../images/logo.png";
 function Header() {
   return (
     <div className="header">
-      <h1><a href="/home">HOME</a></h1>
-      <a href="/home"><img src={logo} alt="Hola"/></a>
+      <a href="/home"><img className="logo" src={logo} alt="Hola"/></a>
       <h1><a href="/family">Protección Familiar</a></h1>
       <h1><a href="/outlawed">Criminalización</a></h1>
     </div>
