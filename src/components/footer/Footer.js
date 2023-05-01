b6f43805ef0f22c525a646c3596b213826bb6d85
Naidr <adrianrodrigueznaranjo@alumno.ieselrincon.es>
2023-05-01 20:58:57 +0100

    media quiere

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,12 +6,11 @@ function Footer() {
     <div className="footer">
       <h1><a href="/">HOME</a></h1>
       <h1>Contact Us</h1>
-      <h1>Email: adrianrodrigueznaranjo@alumno.ieselrincon.es</h1>
+      <h2>Email: adrianrodrigueznaranjo@alumno.ieselrincon.es</h2>
       <h1>More of the profect</h1>
-      <h1>Contact Us</h1>
       <h1><a href="https://github.com/Naidr/projectLgbt">GitHub</a></h1>
       <h1><a href="https://www.figma.com/file/CUkgUyjP8I1X4M79PhQJuF/Rigths?type=design&node-id=0%3A1&t=Id2JGYhhIYfxaWIb-1">Figma</a></h1>
-      <h1>© 2023 Adrián Todos los derechos reservados Política de Privacidad y Cookies</h1>
+      <h2>© 2023 Adrián Todos los derechos reservados Política de Privacidad y Cookies</h2>
     </div>
   );
 }
