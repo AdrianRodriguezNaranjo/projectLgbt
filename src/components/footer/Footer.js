0bef253716f42022f6c63b053148667450bd0b24
Naidr <adrianrodrigueznaranjo@alumno.ieselrincon.es>
2023-05-14 22:16:17 +0100

    RSS bien

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,3 @@
-/* HOME */
 import "./Footer.css";
 
 function Footer() {
@@ -10,6 +9,7 @@ function Footer() {
       <h1>More of the profect</h1>
       <h1><a href="https://github.com/Naidr/projectLgbt">GitHub</a></h1>
       <h1><a href="https://www.figma.com/file/CUkgUyjP8I1X4M79PhQJuF/Rigths?type=design&node-id=0%3A1&t=Id2JGYhhIYfxaWIb-1">Figma</a></h1>
+      <h1><a href="http://127.0.0.1:5500/projectLgbt%2B/src/rss.xml">RSS</a></h1>
       <h2>© 2023 Adrián Todos los derechos reservados Política de Privacidad y Cookies</h2>
     </div>
   );
