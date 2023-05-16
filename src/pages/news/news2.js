ea65794d69937de937072f379ac90efdcce188b9
Naidr <adrianrodrigueznaranjo@alumno.ieselrincon.es>
2023-05-16 16:43:00 +0100

    news descriptions

---
+++ src/pages/news/news2.js
@@ -0,0 +1,29 @@
+import "./news.css";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import NewsServices from "../../services/news.services.js";
+
+function News2() {
+  const news = NewsServices.getNews();
+  const article = news[1];
+
+  return (
+    <>
+      <Header />
+      <div className="News">
+        <article>
+          <div className="imgNews">
+            <img src={article.img} alt={article.new} />
+          </div>
+          <div className="infoNews">
+            <h2>{article.new}</h2>
+            <p>{article.description}</p>
+          </div>
+        </article>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default News2;
